8624959fd5a8ad2bca7e08b8f480b154b075ec4d
VigneshBalakumar <balav391@gmail.com>
2026-05-12 15:32:53 +0530

    mobile update

--- Analyatics.py
+++ Analyatics.py
@@ -1,1 +1,1 @@
-mobile_phone= ["oneplus","apple","samsung","realme","oppo","vivo","Motorola","sony"]
+mobile_phone= ["oneplus","apple","samsung","realme","oppo","vivo","Motorola","sony","google","nothing","lava","micromax"]
