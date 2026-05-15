a7815485ba215899900f6620d8ce88f56440c096
VigneshBalakumar <balav391@gmail.com>
2026-05-15 20:11:49 +0530

    human memory name

--- Analyatics.py
+++ Analyatics.py
@@ -1,1 +1,3 @@
 mobile_phone= ["oneplus","apple","samsung","realme","oppo","vivo","Motorola","sony","google","nothing","lava","micromax"]
+len(mobile_phone)
+names = ["akash","ram","rajesh","roshan","ramya","ramya krishnan","Nisha","jasmine nisha","Gayathri akka","Asha akka"]
